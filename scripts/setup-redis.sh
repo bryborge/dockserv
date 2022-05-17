14d0e72dba36910a5bf7f1ff55676ae78609e8a4
Bryan Borgeson <sonofborge@protonmail.com>
2022-05-17 12:38:56 -0700

    Nextcloud (#29)
    * Add Nextcloud to application stack

--- scripts/setup-redis.sh
+++ scripts/setup-redis.sh
@@ -10,6 +10,7 @@ main() {
   read -rs password
 
   generate_config "$password"
+  generate_secret "$password"
 
   echo "Done."
 }
@@ -25,6 +26,21 @@ generate_config() {
   sed -i "s/<REQUIREPASS>/$1/" redis.conf
 
   popd > /dev/null 2>&1
+}
+
+##
+# Generates the Redis secret.
+#
+generate_secret() {
+  touch ./secrets/redis_default_password
+
+  echo "Generating 'redis_default_password' secret ..."
+  echo "$1" > ./secrets/redis_default_password
+
+  echo "'redis_default_password' file has been generated!"
+  printf "\n"
+
+  echo "Done."
 }
 
 main # Make it so.
